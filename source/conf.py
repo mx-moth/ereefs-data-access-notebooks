ed335cbbb5f66ee8abf1ea10b3fb879b1f001a6b
Tim Heap <tim.heap@csiro.au>
2023-11-20 14:03:33 +1100

    Use Read The Docs Sphinx theme

--- source/conf.py
+++ source/conf.py
@@ -15,6 +15,7 @@ author = 'Tim Heap, Matt Wilde, Sharon Tickell'
 
 extensions = [
     'nbsphinx',
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
@@ -25,5 +26,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
